4fc102d046533a7e945fa2ae1ea118f82a125dc2
Johnatan Caetano <104772832+Johnatan-Caetano@users.noreply.github.com>
2023-05-30 00:22:20 -0300

    Layout
    - Finalização do Layout
    - Implementação das funções dos botões
    - Ao atingir o valor mínimo o botão de sair é desativado

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,116 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext){
+  Widget build(BuildContext context){
     return MaterialApp(
-      home: Container(
-        color: Color(0xffffffff),
-        alignment: Alignment.center,
-        child: Text('Eai bb'),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int count = 0;
+
+  void decrement(){
+    setState(() {
+      count --;
+    });
+    print(count);
+  }
+
+  void increment(){
+     setState(() {
+      count ++;
+    });
+    print(count);
+  }
+
+  bool get isEmpty => count == 0;
+  bool get isFull => count == 20;
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assests/images/image.jpeg'),
+          )
+        ),
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text(
+            isFull? 'Lotado' : 'Pode entrar!',
+            style: TextStyle(
+              fontSize: 30,
+              color: Colors.white,
+              fontWeight: FontWeight.w700,
+            ),
+          ),
+          SizedBox(height: 32,),
+          Text(
+            '$count',
+            style: TextStyle(
+              fontSize: 100,
+              color: Colors.white,
+            ),
+          ),
+           SizedBox(height: 70,),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+               TextButton(
+                onPressed: isEmpty ? null : decrement,
+                style: TextButton.styleFrom(
+                  backgroundColor: isEmpty ? Colors.white.withOpacity(0.2) : Colors.white,
+                  fixedSize: const Size(100, 100),
+                  primary: Colors.black,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24),
+                  )
+                ),
+                child: const Text(
+                  'Saiu',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+
+              const SizedBox(width: 32,),
+               TextButton(  // Adicione o 'const' aqui
+                onPressed: isFull? null : increment,
+                style: TextButton.styleFrom(
+                  backgroundColor:isFull? Colors.white.withOpacity(0.2) : Colors.white,
+                  fixedSize: const Size(100, 100),
+                  primary: Colors.black,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24),
+                  )
+                ),
+                child: const Text(
+                  'Entrou',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),),
     );
   }
 }
